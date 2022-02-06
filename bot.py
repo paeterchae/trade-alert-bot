1c9418ef7f8a141631bb3394720407f32c71edac
Peter Cha <pcha99@gmail.com>
2022-02-05 21:29:58 -0500

    added logging

--- bot.py
+++ bot.py
@@ -1,29 +1,30 @@
 import os
 import random
 from discord.ext import commands
-from tda import auth, client
-import json
+from tda import auth
 from dotenv import load_dotenv
+import logging
+
+#logging
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 load_dotenv()
 
+#const
 token_path = 'token.json'
 api_key = os.getenv('API_KEY')
-redirect_uri = 'https://localhost'
+TOKEN = os.getenv('DISCORD_TOKEN')
 
+#oauth
 try:
     c = auth.client_from_token_file(token_path, api_key)
 except FileNotFoundError:
     print("Token file not found")
 
-r = c.get_price_history('AAPL',
-        period_type=client.Client.PriceHistory.PeriodType.YEAR,
-        period=client.Client.PriceHistory.Period.TWENTY_YEARS,
-        frequency_type=client.Client.PriceHistory.FrequencyType.DAILY,
-        frequency=client.Client.PriceHistory.Frequency.DAILY)
-
-assert r.status_code == 200, r.raise_for_status()
-print(json.dumps(r.json(), indent=4))
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!', help="sdf")
