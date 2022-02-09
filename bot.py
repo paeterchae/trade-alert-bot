5a5bb5c9c480ae65d8164bfb900119ecc6fd14ec
Peter Cha <pcha99@gmail.com>
2022-02-08 22:10:33 -0500

    added embeds for order request and subscription

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 from discord.ext import commands
+from discord import Embed
 from tda import auth
 from dotenv import load_dotenv
 import logging
@@ -17,7 +18,7 @@ load_dotenv()
 TOKEN_PATH = 'token.json'
 API_KEY = os.getenv('API_KEY')
 TOKEN = os.getenv('DISCORD_TOKEN')
-ACCOUNT_ID = os.getenv("ACCOUNT_ID")
+HS_COLOR = 0x50f276
 
 def oauth(TOKEN_PATH, API_KEY):
     try:
@@ -26,10 +27,38 @@ def oauth(TOKEN_PATH, API_KEY):
         print("Token file not found")
 
 def filter(msg):
-    if msg["content"][0]["MESSAGE_TYPE"] == "SUBSCRIBED":
-        return "Account stream has begun"
+    msg_type = msg["content"][0]["MESSAGE_TYPE"]
+    try:
+        msg_data = xmltodict.parse(msg["content"][0]["MESSAGE_DATA"])
+    except:
+        pass
+    if msg_type == "SUBSCRIBED":
+        return format(Embed(title="Trade Alert Bot Activated"))
+        #return "account stream has begun"
+    elif msg_type == "OrderEntryRequest":
+        order = msg_data["OrderEntryRequestMessage"]["Order"]
+        option = order["Security"]["Symbol"].split("_")
+        exp = option[1][:6]
+        cp = "Call" if option[1][6] == "C" else "Put"
+        order_type = order["OrderType"]
+        e = Embed(title="{} {} {} {}/{} {}".format(order["OrderInstructions"], option[0], option[1][7:], exp[:2], exp[2:4], cp), description = "Order Placed")
+        e.add_field(name="Order Type", value=order_type, inline=True)
+        if order_type == "Limit":
+            e.add_field(name="Limit Price", value=order["OrderPricing"]["Limit"], inline=True)
+        e.add_field(name="Size", value=order["OriginalQuantity"])
+        return format(e)
     else:
-        return json.dumps(xmltodict.parse(msg["content"][0]["MESSAGE_DATA"]), indent=4)
+        #return json.dumps(xmltodict.parse(msg["content"][0]["MESSAGE_DATA"]), indent=4)
+        return Embed.Empty
+        #return ""
+
+def format(e=Embed):
+    e.color=HS_COLOR
+    e.set_author(name="Highstrike", url="https://highstrike.com/",
+                icon_url="https://www.highstriketrading.com/hosted/images/78/b23e71dc0b420c80120008ffeb837d/Circle-Logo.png")
+    e.set_thumbnail(url="https://www.highstriketrading.com/hosted/images/78/b23e71dc0b420c80120008ffeb837d/Circle-Logo.png")
+    e.set_footer(text="Highstrike Signals")
+    return e
 
 bot = commands.Bot(command_prefix='!')
 
@@ -42,7 +71,8 @@ async def read_stream(ctx):
     await stream_client.quality_of_service(StreamClient.QOSLevel.EXPRESS)
     
     async def send_response(msg):
-        await ctx.send(filter(msg))
+        await ctx.send(embed=filter(msg))
+        #await ctx.send(filter(msg))
 
     stream_client.add_account_activity_handler(send_response)
     await stream_client.account_activity_sub()
@@ -50,7 +80,7 @@ async def read_stream(ctx):
     while streaming:
         await stream_client.handle_message()
 
-@bot.command(name="end", help="Ends streaming from account")
+@bot.command(name="stop", help="Stops streaming from account")
 async def unsub(ctx):
     global streaming
     streaming = False
@@ -63,7 +93,7 @@ async def unsub(ctx):
 
     streaming = True
 
-    await ctx.send("Account stream has concluded")
+    await ctx.send(embed=format(Embed(title="Trade Alert Bot Deactivated")))
 
 @bot.event
 async def on_ready():
