cddefb50390edd8e20ae2fc49ab35d45d61f97ef
Peter Cha <pcha99@gmail.com>
2022-04-03 15:59:44 -0400

    added order_fill msg functionality

--- bot.py
+++ bot.py
@@ -49,7 +49,7 @@ def parser(msg_data, msg_type):
     limit_price = None if order_type != "Limit" else "{:0.2f}".format(float(order["OrderPricing"]["Limit"]))
     bid = "{:0.2f}".format(float(order["OrderPricing"]["Bid"]))
     ask = "{:0.2f}".format(float(order["OrderPricing"]["Ask"]))
-    return bs, ticker, strike, exp, cp, order_type, acc_value, num_contracts, limit_price, symbol, bid, ask
+    return bs, ticker, strike, exp, cp, order_type, acc_value, num_contracts, limit_price, bid, ask
         
 def filter(msg):
     msg_type = msg["content"][0]["MESSAGE_TYPE"]
@@ -59,23 +59,24 @@ def filter(msg):
         return None
     else:
         msg_data = xmltodict.parse(msg["content"][0]["MESSAGE_DATA"])
-        bs, ticker, strike, exp, cp, order_type, acc_value, num_contracts, limit_price, symbol, bid, ask = parser(msg_data, msg_type)
+        bs, ticker, strike, exp, cp, order_type, acc_value, num_contracts, limit_price, bid, ask = parser(msg_data, msg_type)
         e = Embed(title="{} {} {} {} {}".format(bs, ticker, strike, exp, cp))
         e.add_field(name="Order Type", value=order_type, inline=True)
+        e.color = 0xFFFF00
         #position size only visible if limit order
         if order_type == "Limit":
             e.add_field(name="Limit Price", value=limit_price, inline=True)
-            e.add_field(name="Position Size", value=str(int(float(limit_price) * 10000.0 * num_contracts / float(acc_value))) + "%")
+            if bs == "Buy":
+                e.add_field(name="Position Size", value=str(int(float(limit_price) * 10000.0 * num_contracts / float(acc_value))) + "%")
         elif order_type == "Market":
             e.add_field(name="Bid", value= bid, inline=True)
             e.add_field(name="Ask", value=ask, inline=True)
-            e.add_field(name="Position Size", value=str(int((float(bid)+float(ask))/2 * 10000.0 * num_contracts / float(acc_value))) + "%")
+            if bs == "Buy":
+                e.add_field(name="Position Size", value=str(int((float(bid)+float(ask))/2 * 10000.0 * num_contracts / float(acc_value))) + "%")
         if msg_type == "OrderEntryRequest":
             e.description = "Order Placed"
-            e.color = 0x50f276 if (bs == "Buy") else 0xFF0000
             return format(e)
         elif msg_type == "OrderCancelReplaceRequest":
-            e.color = 0x50f276 if (bs == "Buy") else 0xFF0000
             e.description = "Replacement Order Placed"
             return format(e)
         elif msg_type == "UROUT":
@@ -167,17 +168,37 @@ async def slow_count():
     channel = bot.get_channel(int(CHANNEL_ID))
     await channel.send(slow_count.current_loop)
 
-async def order_fill(order, action, prev=None):
-    #symbol, longQuantity, marketValue, averagePrice, currentDayProfitLossPercentage
-    channel = bot.get_channel(int(CHANNEL_ID))
-    await channel.send("Filled")
+async def order_fill(order, action, acc_value, prev=None):
+    if order["instrument"]["assetType"] == "OPTION":
+        symbol = order["instrument"]["symbol"]
+        option = symbol.split("_")
+        ticker = option[0]
+        strike = option[1][7:]
+        exp = option[1][:6][:2] + "/" + option[1][:6][2:4] + "/" + option[1][:6][4:6]
+        cp = "Call" if option[1][6] == "C" else "Put"
+        if action == "Trim":
+            trim_percentage = str((1.0 - order["longQuantity"]/prev["longQuantity"]) * 100.0) + "%"
+            e = Embed(title="{} {} {} {} {} {}".format(action, trim_percentage, ticker, strike, exp, cp))
+            e.add_field(name="Position Left", value=str(order["marketValue"] / acc_value * 100.0) + "%", inline=True)
+        else:
+            e = Embed(title="{} {} {} {} {}".format(action, ticker, strike, exp, cp))
+        e.add_field(name="Average Cost", value=str(order["averagePrice"]))
+        e.add_field(name="Market Price", value=str(order["marketValue"]/order["longQuantity"]/100.0))
+        e.add_field(name="P/L", value=str(order["currentDayProfitLossPercentage"])+"%")
+        if action == "Buy":
+            e.add_field(name="Total Position", value=str(order["marketValue"] / acc_value * 100.0) + "%", inline=True)
+        e.color = 0x50f276 if action == "Buy" else 0xFF0000
+        channel = bot.get_channel(int(CHANNEL_ID))
+        await channel.send(embed=format(e))
 
 @tasks.loop(seconds=1)
 async def update_positions():
     global curr_positions
     tmp = copy.deepcopy(curr_positions)
     try:
-        new_positions = client.get_account(ACCOUNT_ID, fields=Client.Account.Fields.POSITIONS).json()["securitiesAccount"]["positions"]
+        account = client.get_account(ACCOUNT_ID, fields=Client.Account.Fields.POSITIONS).json()["securitiesAccount"]
+        new_positions = account["positions"]
+        acc_value = account["currentBalances"]["liquidationValue"]
         tracked_positions = []
         #addition or update
         for pos in new_positions:
@@ -186,24 +207,24 @@ async def update_positions():
             tracked_positions.append(symbol)
             curr_positions[symbol] = pos
             if tmp.get(symbol) == None or amt > tmp[symbol]["longQuantity"]:
-                await order_fill(pos, "Buy")
+                await order_fill(pos, "Buy", acc_value)
             elif amt < tmp[symbol]["longQuantity"]:
-                await order_fill(pos, "Trim", tmp)
+                await order_fill(pos, "Trim", acc_value, tmp)
         #removal
         for symbol in tmp.keys():
             if symbol not in tracked_positions:
                 del curr_positions[symbol]
                 if tmp[symbol]["currentDayProfitLossPercentage"] > 0:
-                    await order_fill(tmp[symbol], "Exit")
+                    await order_fill(tmp[symbol], acc_value, "Exit")
                 else:
-                    await order_fill(tmp[symbol], "Cut")
+                    await order_fill(tmp[symbol], acc_value, "Cut")
     except KeyError:
         if tmp != {}:
             curr_positions = {}
             for symbol in tmp.keys():
                 if pos["currentDayProfitLossPercentage"] > 0:
-                    await order_fill(tmp[symbol], "Exit")
+                    await order_fill(tmp[symbol], acc_value, "Exit")
                 else:
-                    await order_fill(tmp[symbol], "Cut")
+                    await order_fill(tmp[symbol], acc_value, "Cut")
 
 bot.run(TOKEN)
